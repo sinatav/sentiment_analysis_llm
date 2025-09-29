436debfaa0c9c86dd22a9b21d002c6d1a99c2cfc
Sina <stavakkoli1999@gmail.com>
2025-09-29 20:37:24 +0330

    Fix: switched to langchain deepseek

--- src/llm_client.py
+++ src/llm_client.py
@@ -1,12 +1,12 @@
-from langchain_community.llms import DeepSeek
+from langchain_deepseek import ChatDeepSeek
 from src.config import DEEPSEEK_API_KEY
 
 def get_llm():
     if not DEEPSEEK_API_KEY:
         raise ValueError("DeepSeek API key not found. Set it in .env file.")
     
-    return DeepSeek(
-        model="gpt-40-mini",
+    return ChatDeepSeek(
+        model="deepseek-chat",
         api_key=DEEPSEEK_API_KEY,
         temperature=0.0
     )
